2ea01e55d9a3539a7f738ea60d9d3dae6e2e0802
James Shapiro <jamesshapiro@gmail.com>
2020-10-10 23:51:27 -0700

    fix Person styling with CSS scoped modules, and finish 05 styling unit

--- react-complete-guide-05-styling-redo-2/src/Person/Person.js
+++ react-complete-guide-05-styling-redo-2/src/Person/Person.js
@@ -1,23 +1,10 @@
 import React from 'react';
 
-//import classes from './Person.module.css'
-
-/*const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 4px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`*/
+import classes from './Person.module.css'
 
 const person = (props) => {
     return (
-        <div>
+        <div className = {classes.Person}>
             <p onClick={props.click}>I'm a Person! My name is {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
