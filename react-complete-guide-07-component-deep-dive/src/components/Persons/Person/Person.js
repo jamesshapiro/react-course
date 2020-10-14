e2cf034061afc988f4b7c9da1bab6dad1295043a
James Shapiro <jamesshapiro@gmail.com>
2020-10-14 15:54:39 -0700

    rendering adjacent JSX Elements with React.Fragment instead of Auxiliary HOC

--- react-complete-guide-07-component-deep-dive/src/components/Persons/Person/Person.js
+++ react-complete-guide-07-component-deep-dive/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.module.css';
 import Aux from '../../../hoc/Auxiliary'
 
@@ -6,11 +6,11 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>I'm a Person! My name is {this.props.name} and I am {this.props.age} years old!</p>
                 <p> {this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </Fragment>
             
         )
         
