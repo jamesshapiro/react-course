4176cb7583fb8a55eb929385f293fb2fb09195c1
James Shapiro <jamesshapiro@gmail.com>
2020-10-14 15:52:29 -0700

    rendering adjacent JSX Elements with Auxiliary HOC

--- react-complete-guide-07-component-deep-dive/src/components/Persons/Person/Person.js
+++ react-complete-guide-07-component-deep-dive/src/components/Persons/Person/Person.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import classes from './Person.module.css';
+import Aux from '../../../hoc/Auxiliary'
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
-        return [
-            <p key="i1" onClick={this.props.click}>I'm a Person! My name is {this.props.name} and I am {this.props.age} years old!</p>,
-            <p key="i2">{this.props.children}</p>,
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
-        ]
+        return (
+            <Aux>
+                <p onClick={this.props.click}>I'm a Person! My name is {this.props.name} and I am {this.props.age} years old!</p>
+                <p> {this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+            
+        )
+        
     }
 
 }
