bbe44be16fa189e12f63d81b0117ffd3aab74ce5
James Shapiro <jamesshapiro@gmail.com>
2020-10-14 18:35:52 -0700

    setting state correctly for state updates that depend on the old state module 07

--- react-complete-guide-07-component-deep-dive/src/containers/App.js
+++ react-complete-guide-07-component-deep-dive/src/containers/App.js
@@ -21,7 +21,8 @@ class App extends Component {
     ],
     tful: false,
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -55,7 +56,12 @@ class App extends Component {
     person.name = event.target.value
     const persons = [...this.state.persons]
     persons[personIndex] = person
-    this.setState({ persons: persons })
+    this.setState((prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1
+      }
+    })
   }
 
   deletePersonHandler = (personIndex) => {
