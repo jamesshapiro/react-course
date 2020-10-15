2c98f36578f876958dab6f7d85cee431f9a58d83
James Shapiro <jamesshapiro@gmail.com>
2020-10-14 18:28:58 -0700

    passing unknown props module 07

--- react-complete-guide-07-component-deep-dive/src/components/Persons/Person/Person.js
+++ react-complete-guide-07-component-deep-dive/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import classes from './Person.module.css';
 import Aux from '../../../hoc/Auxiliary'
+import withClass from '../../../hoc/withClass'
 
 class Person extends Component {
     render() {
@@ -18,4 +19,4 @@ class Person extends Component {
 
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
