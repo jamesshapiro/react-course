3eea1db32f1ba7e28789f4e540fc6fb8fb6ab116
James Shapiro <jamesshapiro@gmail.com>
2020-10-15 12:22:19 -0700

    using the contextType and useContext instead of <AuthContext.Consumer> in both class-based and functional components module-07

--- react-complete-guide-07-component-deep-dive/src/components/Persons/Persons.js
+++ react-complete-guide-07-component-deep-dive/src/components/Persons/Persons.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import Person from './Person/Person';
-import AuthContext from '../../context/auth-context';
 
 class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state) {
