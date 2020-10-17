519c273853c7c01dc2dfd66cd11679654bf52dd3
James Shapiro <jamesshapiro@gmail.com>
2020-10-16 17:51:57 -0700

    improve performance by adding shouldComponentUpdate check to modal, also addd DrawerToggle styling class that I forgot to add earlier

--- burger-builder-08-02/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-08-02/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>
-                        {igKey}: {props.ingredients[igKey]}
-                    </span>
-                </li>
-            );
-        });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button
-                btnType="Danger"
-                clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button
-                btnType="Success"
-                clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+class OrderSummary extends Component {
+    // This could be a functional component, doesn't have to be a class.
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+    render() {
+        const ingredientSummary = Object.keys( this.props.ingredients )
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>
+                            {igKey}: {this.props.ingredients[igKey]}
+                        </span>
+                    </li>
+                );
+            });
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button
+                    btnType="Danger"
+                    clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button
+                    btnType="Success"
+                    clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
