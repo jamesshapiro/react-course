ad96f56e2bc0cc0a7fba8e18fc8d9fbc6e93b7a7
James Shapiro <jamesshapiro@gmail.com>
2020-10-17 23:27:09 -0700

    extracting route parameters

--- routing-11/src/containers/Blog/FullPost/FullPost.js
+++ routing-11/src/containers/Blog/FullPost/FullPost.js
@@ -8,12 +8,14 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidUpdate() {
-        if (this.props.id) {
-            if (this.state.loadedPost && this.state.loadedPost.id === this.props.id) {
+    componentDidMount() {
+        console.log(this.props);
+
+        if (this.props.match.params.id) {
+            if (this.state.loadedPost && this.state.loadedPost.id === this.props.match.params.id) {
                 return;
             }
-            axios.get('/posts/' + this.props.id)
+            axios.get('/posts/' + this.props.match.params.id)
                 .then(response => {
                     //console.log(response)
                     this.setState({loadedPost: response.data});
@@ -22,7 +24,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.id)
             .then(response => {
                 console.log(response);
             });
