fa4d0e2d895814966366198dd1a3b291cd454be5
James Shapiro <jamesshapiro@gmail.com>
2020-10-17 17:36:30 -0700

    switching between pages

--- routing-11/src/containers/Blog/Blog.js
+++ routing-11/src/containers/Blog/Blog.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-// import axios from 'axios'
-import axios from '../../axios'
-import Posts from './Posts/Posts'
+// import axios from 'axios';
+import axios from '../../axios';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost'
+
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 
@@ -19,7 +22,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home 2</h1>}/> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
