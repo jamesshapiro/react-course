fc99941d4187af29e0ea6eac929321c0de43773b
James Shapiro <jamesshapiro@gmail.com>
2020-10-17 23:23:07 -0700

    passing route parameters

--- routing-11/src/containers/Blog/Blog.js
+++ routing-11/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost'
+import FullPost from './FullPost/FullPost'
 
 import { Route, NavLink } from 'react-router-dom';
 
@@ -37,6 +38,7 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>}/> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
