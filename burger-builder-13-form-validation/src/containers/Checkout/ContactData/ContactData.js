0b79a07b906fd6e6cb3cd63be2dfdc015f936b63
James Shapiro <jamesshapiro@gmail.com>
2020-10-31 21:31:32 -0700

    Dynamically Create Inputs based on JS Config

--- burger-builder-13-form-validation/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-13-form-validation/src/containers/Checkout/ContactData/ContactData.js
@@ -82,12 +82,23 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
         let form = (
             <form>
-                <Input elemeentType="..." elementConfig="..." value="..." />
-                <Input inputType="input" type="text" name="email" placeholder="Your Email" />
-                <Input inputType="input" type="text" name="street" placeholder="Street" />
-                <Input inputType="input" type="text" name="postal" placeholder="Postal Code" />
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                    />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
