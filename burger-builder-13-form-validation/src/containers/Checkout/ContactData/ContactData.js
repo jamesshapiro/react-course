8508d468113e761fb6ff0ed7668d5cc6bf8abf4f
James Shapiro <jamesshapiro@gmail.com>
2020-11-01 03:09:28 -0800

    fix a couple bugs to finish validation module

--- burger-builder-13-form-validation/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-13-form-validation/src/containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,8 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'},
                     ]
                 },
-                value: '',
+                validation: {},
+                value: 'fastest',
                 valid: true
             },
         },
