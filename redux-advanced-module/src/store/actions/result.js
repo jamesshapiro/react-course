4c17ad0fb1c5a051829bd2063f4eb3446aa5be91
James Shapiro <jamesshapiro@gmail.com>
2020-11-07 23:40:40 -0800

    Using Utility Functions

--- redux-advanced-module/src/store/actions/result.js
+++ redux-advanced-module/src/store/actions/result.js
@@ -1,15 +1,18 @@
 import * as actionTypes from './actionTypes'
 
 export const saveResult = (res) => {
+    //const updatedResult = res * 2;
     return {
         type: actionTypes.STORE_RESULT,
         result: res
-    }
+    };
 }
 
 export const storeResult = (res) => {
-    return dispatch => {
+    return (dispatch, getState) => {
         setTimeout(() => {
+            //const oldCounter = getState().ctr.counter;
+            //console.log(oldCounter);
             dispatch(saveResult(res));
         }, 2000 );
     }
