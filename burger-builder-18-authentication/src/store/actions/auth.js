73f77dcb2992927b27ffd0648cc101825f85a596
James Shapiro <jamesshapiro@gmail.com>
2020-11-09 02:12:45 -0800

    Adding Sign-in

--- burger-builder-18-authentication/src/store/actions/auth.js
+++ burger-builder-18-authentication/src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDGe1-zpjc6vRSImvKIf_CZMfh1af2rCvk', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDGe1-zpjc6vRSImvKIf_CZMfh1af2rCvk';
+        if (!isSignUp) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDGe1-zpjc6vRSImvKIf_CZMfh1af2rCvk'
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
