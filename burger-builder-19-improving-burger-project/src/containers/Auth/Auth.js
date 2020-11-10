45265ddfde1bd59001e5e3c39eef409b826e8191
James Shapiro <jamesshapiro@gmail.com>
2020-11-10 02:13:13 -0800

    Using updateObject in the entire app

--- burger-builder-19-improving-burger-project/src/containers/Auth/Auth.js
+++ burger-builder-19-improving-burger-project/src/containers/Auth/Auth.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/UI/Input/Input';
-import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index';
+import { updateObject } from '../../shared/utility'
 
 class Auth extends Component {
     state = {
@@ -68,15 +68,13 @@ class Auth extends Component {
     }
 
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-            }
-        };
+            })
+        });
         this.setState({ controls: updatedControls });
     }
 
--- burger-builder-19-improving-burger-project/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-19-improving-burger-project/src/containers/Checkout/ContactData/ContactData.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Button from '../../../components/UI/Button/Button'
-import Spinner from '../../../components/UI/Spinner/Spinner'
-import classes from './ContactData.module.css'
-import axios from '../../../axios-orders'
-import Input from '../../../components/UI/Input/Input'
-import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
-import * as actions from '../../../store/actions/index'
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
+import { updateObject } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = {
@@ -132,19 +133,19 @@ class ContactData extends Component {
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        };
-        const updatedOrderFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        };
-        updatedOrderFormElement.value = event.target.value;
-        updatedOrderFormElement.valid = this.checkValidity(
-            updatedOrderFormElement.value,
-            updatedOrderFormElement.validation
-        );
-        updatedOrderFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedOrderFormElement;
+        
+        const updatedOrderFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            valid: this.checkValidity(
+                event.target.value,
+                this.state.orderForm[inputIdentifier].validation
+            ),
+            touched: true
+        });
+
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedOrderFormElement
+        });
         
         let formIsValid = true;
         for (let inputIdentifier in updatedOrderForm) {
