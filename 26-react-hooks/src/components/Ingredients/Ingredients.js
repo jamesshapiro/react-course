6223a53797ed1692e1867a840e8676fa117edc73
James Shapiro <jamesshapiro@gmail.com>
2020-11-15 01:33:22 -0800

    More on useEffect()

--- 26-react-hooks/src/components/Ingredients/Ingredients.js
+++ 26-react-hooks/src/components/Ingredients/Ingredients.js
@@ -23,6 +23,10 @@ const Ingredients = () => {
       });
   }, []);
 
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  }, [userIngredients]);
+
   const addIngredientHandler = (ingredient) => {
     fetch("https://react-hooks-update-a1401.firebaseio.com/ingredients.json", {
       method: "POST",
