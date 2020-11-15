926a50cc8f6bca33ee98fadffc8ee71a11c4005f
James Shapiro <jamesshapiro@gmail.com>
2020-11-15 01:29:23 -0800

    useEffect() and Loading Data

--- 26-react-hooks/src/components/Ingredients/Ingredients.js
+++ 26-react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,15 +7,29 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch("https://react-hooks-update-a1401.firebaseio.com/ingredients.json")
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
   const addIngredientHandler = (ingredient) => {
     fetch("https://react-hooks-update-a1401.firebaseio.com/ingredients.json", {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" },
     })
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((responseData) => {
         setUserIngredients((prevIngredients) => [
           ...prevIngredients,
